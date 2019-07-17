93ea05d630b094ea37d238bfbf4d150d22059ce8
Albert <krasniqi.albert24@gmail.com>
2019-07-18 00:25:39 +0200

    reindentation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Redirect } from 'react-router-dom';
 
-
-//context consumer
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
 class Login extends React.Component {
     state = {
         login: {
@@ -16,7 +11,6 @@ class Login extends React.Component {
 
     };
 
-
     formSubmitted = e => {
         // this.setState({isLoadingDisplayed: true});
         e.preventDefault();
@@ -24,8 +18,6 @@ class Login extends React.Component {
         fetch('http://localhost:3001/users/authenticate', {
             method: 'POST',
             headers: {
-                // 'Accept': 'application/x-www-form-urlencoded',
-                // 'Content-Type': 'application/x-www-form-urlencoded',
                 'Content-Type': "application/json"
             },
             body: JSON.stringify({
@@ -34,24 +26,12 @@ class Login extends React.Component {
             })
         })
             .then(() => this.setState({ redirect: true }))
-
-            
             .catch(err => console.error('Caught error: ', err));
-
-
-
     };
-
-// <Redirect
-// to={{
-//     pathname: "/accueil",
-//     state: { resultat: res.json() }
-// }}
-// />
 
     inputValueChanged = e => {
 
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         this.setState(prevState => ({
             login: {
                 ...prevState.login,
@@ -60,38 +40,34 @@ class Login extends React.Component {
         }));
     };
 
+    render() {
 
-
-
-        render(){
-
-            const { redirect } = this.state;
-            if (redirect) {
-                console.log("aze");
-                return <Redirect to='/accueil'/>;
-            }
-            return (
-                <div className="col-md-6 col-md-offset-3">
-                    <h2>Connexion</h2>
-                    <form name="form" method="POST" onSubmit={this.formSubmitted}>
-                        <div className="form-group">
-                            <label>email</label>
-                            <input name="email" onChange={this.inputValueChanged} type="text" className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label>Mot de Passe</label>
-                            <input name="password" onChange={this.inputValueChanged} type="password"
-                                   className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-primary">Connexion</button>
-                        </div>
-                    </form>
-                </div>
-                );
-
+        const { redirect } = this.state;
+        if (redirect) {
+            console.log("aze");
+            return <Redirect to='/accueil' />;
         }
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Connexion</h2>
+                <form name="form" method="POST" onSubmit={this.formSubmitted}>
+                    <div className="form-group">
+                        <label>email</label>
+                        <input name="email" onChange={this.inputValueChanged} type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Mot de Passe</label>
+                        <input name="password" onChange={this.inputValueChanged} type="password"
+                            className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">Connexion</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 
-    };
+};
 
-  export default Login
+export default Login
