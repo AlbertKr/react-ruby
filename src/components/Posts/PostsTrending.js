bd7c5d6932295ff9ca65ce87219a7ca7ef582352
Albert <krasniqi.albert24@gmail.com>
2019-07-19 00:36:24 +0200

    ajout delete

--- src/components/Posts/PostsTrending.js
+++ src/components/Posts/PostsTrending.js
@@ -2,52 +2,68 @@ import React, { Component } from 'react';
 
 class PostsTrending extends Component {
 
-    // state = {
-    //     articles: {
-    //         article1: {
-    //             user: {
-    //                 email: 'albert.test@gg.com',
-    //                 pseudo: 'Alberto'
-    //             },
-    //             content: "J'ai finis le React !"
-    //         },
-    //         article2: {
-    //             user: {
-    //                 email: 'joris.test@gg.com',
-    //                 pseudo: 'Jojo'
-    //             },
-    //             content: "J'ai finis le Ruby !"
-    //         },
-    //         article3: {
-    //             user: {
-    //                 email: 'louis.test@gg.com',
-    //                 pseudo: 'Loulou'
-    //             },
-    //             content: "J'ai finis le Node !"
-    //         }
-    //     }
-    // };
     state = {
-        articles: []
-
-
+        articles: {
+            article1: {
+                user: {
+                    email: 'albert.test@gg.com',
+                    pseudo: 'Alberto'
+                },
+                content: "J'ai finis le React !",
+                id: 1
+            },
+            article2: {
+                user: {
+                    email: 'joris.test@gg.com',
+                    pseudo: 'Jojo'
+                },
+                content: "J'ai finis le Ruby !",
+                id: 2
+            },
+            article3: {
+                user: {
+                    email: 'louis.test@gg.com',
+                    pseudo: 'Loulou'
+                },
+                content: "J'ai finis le Node !",
+                id: 3
+            }
+        }
     };
+    // state = {
+    //     articles: []
 
-    componentDidMount() {
-        fetch('http://localhost:3001/articles', {
-            method: 'GET',
+
+    // };
+
+    // componentDidMount() {
+    //     fetch('http://localhost:3001/articles', {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': "application/json"
+    //         }
+    //     })
+    //         .then(res => res.json())
+    //         .then((data) => {
+    //             this.setState({ articles: data.articles });
+    //         })
+    //         .then(() => this.setState({redirect: true}))
+    //         .catch(err => console.error('Caught error: ', err));
+    // };
+
+    handleDelete(id_article){
+        fetch('http://localhost:3001/articles/delete', {
+            method: 'POST',
             headers: {
                 'Content-Type': "application/json"
-            }
-        })
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ articles: data.articles });
+            },
+            body: JSON.stringify({
+                id_article: id_article
             })
-            .then(() => this.setState({redirect: true}))
+        })
+            .then(() => this.setState({ redirect: true }))
             .catch(err => console.error('Caught error: ', err));
-    };
-
+    }
 
     render() {
         if( this.state.articles !== undefined ){
@@ -60,11 +76,9 @@ class PostsTrending extends Component {
                         <div>
                             <p> Contenu de l'article : </p>
                             <p>{this.state.articles[carID].content}</p>
-
-
-                            <img src={ require("../../img/" + this.state.articles[carID].image)}/>
-                        }
+                            {/* <img src={ require("../../img/" + this.state.articles[carID].image)}/> */}
                         </div>
+                        <button onClick={() => this.handleDelete(this.state.articles[carID].id)}> Supprimer l'article </button>
                     </div>
                 ))}
                 </div>
